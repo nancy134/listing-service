3b96b24f5917496e8d8ab78ecf464d419380bed2
Nancy Piedra <nancy_piedra@yahoo.com>
2020-01-25 20:54:50 +0000

    Create listin

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const express = require('express');
+const Sequelize = require('sequelize');
+const ListingModel = require('./models/listing');
+const bodyParser = require('body-parser');
 
 // Constants
 const PORT = 8080;
@@ -8,9 +11,36 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('listing-api.phowma.com\n');
 });
 
+app.get('/listings', (req, res) => {
+    const sequelize = new Sequelize(process.env.DATABASE_URL);
+    const Listing = ListingModel(sequelize, Sequelize);
+    Listing.findAll().then(listings => res.json(listings))
+});
+
+app.post('/listing', (req, res) => {
+   var tenant = req.body.tenant;
+   var email = req.body.email;
+   var address = req.body.address;
+   var city = req.body.city;
+   var state = req.body.state;
+   const sequelize = new Sequelize(process.env.DATABASE_URL);
+   const Listing = ListingModel(sequelize, Sequelize);
+   Listing.create({
+       tenant: tenant,
+       email: email,
+       address: address,
+       city: city,
+       state: state
+   }).then(listing => {
+       console.log("listing.id: "+listing.id);
+       res.json(listing);
+   });
+});
 app.listen(PORT, HOST);
